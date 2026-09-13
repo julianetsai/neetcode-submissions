3f881b295a3c406b990ca825749fb182884a66f1
julianetsai <jubo23@gmail.com>
2026-09-13 13:12:44 -0700

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # map with value, index
+        # as you go through nums, look for target-k
+        # return target-k index, and curr index
+        seenIdx = {}
+
+        for i,n in enumerate(nums):
+            diff = target - n
+            if diff in seenIdx:
+                return [seenIdx[diff], i]
+            seenIdx[n]=i
+
+        return []
+
+        # O(n), O(n)
