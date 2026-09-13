207a84be10808dc4d338c01b66f207f2933aac0c
julianetsai <jubo23@gmail.com>
2026-09-13 12:52:34 -0700

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # make a map of s char, freq, then for t subtract. if its zero remove.  map   should be empty at the end
+        freq = defaultdict(int)
+        for s_ch in s:
+            freq[s_ch]+=1
+        for t_ch in t:
+            freq[t_ch]-=1
+            if freq[t_ch]==0:
+                del freq[t_ch]
+        return len(freq)==0
+
+
